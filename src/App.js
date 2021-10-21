f9ac6644233527bbd6e3912fee21802f8adc5fe7
Mario Rojas Catalan <77466868+mariorojasac@users.noreply.github.com>
2021-10-21 00:04:34 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// IMPORT useSta
+import React, { useState } from "react";
+import "./styles.css";
+// IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from "./imageData";
+
+export default function App() {
+  // USE useState TO CREATE  [bigImage, setBigImage]
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
+  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+
+  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+
+  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
+  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
+  const images = imagesArr.map((image, city) => {
+    return (
+      <img
+        src={image.img}
+        className="thumb"
+        alt={city.city}
+        onClick={() => setBigImage(image.img)}
+      />
+    );
+  });
+
+  return (
+    <div className="App">
+      <h1>Cities Of The World</h1>
+      <div id="wrapper">
+        <div id="thumbnails">{images}</div>
+
+        <img src={bigImage} id="bigimage" alt="bigImage" />
+      </div>
+    </div>
+  );
+}
